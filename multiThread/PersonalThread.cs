37f7f496a1d7b61d2291fb6cd98e41678de04cc2
Erkan Liman <limanerkan@outlook.com>
2017-03-28 00:45:45 +0300

    Add project files.

---
+++ multiThread/PersonalThread.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+
+namespace multiThread
+{
+    class PersonalThread
+    {
+        public int sayac;
+        public Thread myThread;
+        //constructor
+        public PersonalThread(string name)
+        {
+            sayac = 0;
+            myThread = new Thread(new ThreadStart(this.run));
+            myThread.Name = name;
+            myThread.Start();
+        }
+
+        public void run()
+        {
+            Console.WriteLine(myThread.Name + "başladı");
+            do
+            {
+                Thread.Sleep(500);
+                Console.WriteLine("sayac:" + sayac);
+                sayac++;
+            } while (sayac < 10);
+            Console.WriteLine(myThread.Name + " bitti");
+        }
+    }
+}
---
+++ multiThread/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+
+namespace multiThread
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PersonalThread thread1 = new PersonalThread("thread1");
+
+
+            Thread.Sleep(2000);
+
+            thread1.myThread.Suspend();
+            Console.WriteLine(thread1.myThread.Name + " durduruldu.");
+            Thread.Sleep(1000);
+            thread1.myThread.Resume();
+            Console.WriteLine(thread1.myThread.Name + " devam ettirildi.");
+        }
+    }
+}
